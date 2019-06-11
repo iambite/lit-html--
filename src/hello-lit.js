0210cd5d37265c9d1b6ee09e9b00c7264378ceb4
iambite <wne@iambite.com>
2019-06-12 06:34:44 +0800

    2 files modified

--- src/hello-lit.js
+++ src/hello-lit.js
@@ -1,34 +1,5 @@
 import { LitElement, html } from '@polymer/lit-element';
-import { SharedStyles } from './SharedStyles.js';
-
 class HelloLit extends LitElement {
-  static get properties() {
-    return {
-      name: { type: String }
-    }
-  }
-  constructor() {
-    super();
-    this.author = { name: 'Gooloan', logo: 'http://www.myfreephotoshop.com/wp-content/uploads/2013/08/669.jpg' };
-    this.items = [
-      { title: 'loan', image: 'http://www.myfreephotoshop.com/wp-content/uploads/2013/08/669.jpg', description: '的搜索結果集中找到該頁面，則社交個人資料標記應該創建一個或多個麵包屑路徑，而不會有' },
-      { title: 'loan', image: 'http://www.myfreephotoshop.com/wp-content/uploads/2013/08/669.jpg', description: '的搜索結果集中找到該頁面，則社交個人資料標記應該創建一個或多個麵包屑路徑，而不會有'},
-    ];
-  }
-
-  render() {
-    return html`
-      <author-profile
-        .author=${this.author}
-        .items=${this.items}>
-      </author-profile>
-    `;
-  }
-}
-
-customElements.define('hello-lit', HelloLit);
-
-class AuthorProfile extends LitElement {
   static get properties() {
     return {
       author: { type: Object },
@@ -49,4 +20,4 @@ class AuthorProfile extends LitElement {
   }
 }
 
-customElements.define('author-profile', AuthorProfile);
+customElements.define('hello-lit', HelloLit);
--- src/index.js
+++ src/index.js
@@ -5,19 +5,24 @@ import { SharedStyles } from './SharedStyles.js';
 class MyApp extends LitElement {
   static get properties() {
     return {
-      author: { type: Object },
-      items: { type: Array },
-    };
-  }  
-  render(){
-    const name = 'LitElement';
-    
+      name: { type: String }
+    }
+  }
+  constructor() {
+    super();
+    this.author = { name: 'Gooloan', logo: 'http://www.myfreephotoshop.com/wp-content/uploads/2013/08/669.jpg' };
+    this.items = [
+      { title: 'loan', image: 'http://www.myfreephotoshop.com/wp-content/uploads/2013/08/669.jpg', description: '的搜索結果集中找到該頁面，則社交個人資料標記應該創建一個或多個麵包屑路徑，而不會有' },
+      { title: 'loan', image: 'http://www.myfreephotoshop.com/wp-content/uploads/2013/08/669.jpg', description: '的搜索結果集中找到該頁面，則社交個人資料標記應該創建一個或多個麵包屑路徑，而不會有'},
+    ];
+  }
+
+  render() {
     return html`
-      ${SharedStyles}
-      <hello-lit .name=${name}></hello-lit>
-      <p>
-        Start editing to see some magic happen :)
-      </p>
+      <hello-lit
+        .author=${this.author}
+        .items=${this.items}>
+      </hello-lit>
     `;
   }
 }
