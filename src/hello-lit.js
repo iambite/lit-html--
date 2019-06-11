fa4f070ec7afa10ac7524d2d09dbe492c8c973c4
iambite <wne@iambite.com>
2019-06-12 06:31:21 +0800

    1 file modified

--- src/hello-lit.js
+++ src/hello-lit.js
@@ -27,3 +27,26 @@ class HelloLit extends LitElement {
 }
 
 customElements.define('hello-lit', HelloLit);
+
+class AuthorProfile extends LitElement {
+  static get properties() {
+    return {
+      author: { type: Object },
+      items: { type: Array },
+    };
+  }
+
+  render() {
+    return html`
+      <img src="${this.author.logo}" alt="${this.author.name}">   
+      <p>${this.author.name}</p>
+      <div>${this.items.map(item => html`
+        <li>
+          <img src="${item.image}" alt="${item.title}">
+          <p>${item.description}</p>
+        </li>`)}
+    `;
+  }
+}
+
+customElements.define('author-profile', AuthorProfile);
