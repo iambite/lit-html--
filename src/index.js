f01c9059c00d7ad761c0280f2a357f878928eefd
iambite <wne@iambite.com>
2019-06-12 06:51:06 +0800

    1 file modified

--- src/index.js
+++ src/index.js
@@ -6,21 +6,21 @@ class MyApp extends LitElement {
 
   constructor() {
     super();
-    this.author = { name: 'Gooloan', logo: 'http://www.myfreephotoshop.com/wp-content/uploads/2013/08/669.jpg' };
+    this.author = { name: 'Gooloan', logo: 'https://storage.googleapis.com/gooloan-images/gooloan-1.jpg' };
     this.items = [
-      { title: 'loan', image: 'http://www.myfreephotoshop.com/wp-content/uploads/2013/08/669.jpg', description: '的搜索結果集中找到該頁面，則社交個人資料標記應該創建一個或多個麵包屑路徑，而不會有' },
-      { title: 'loan', image: 'http://www.myfreephotoshop.com/wp-content/uploads/2013/08/669.jpg', description: '的搜索結果集中找到該頁面，則社交個人資料標記應該創建一個或多個麵包屑路徑，而不會有'},
+      { title: 'loan', image: 'https://storage.googleapis.com/gooloan-images/gooloan-2.jpg', description: '的搜索結果集中找到該頁面，則社交個人資料標記應該創建一個或多個麵包屑路徑，而不會有' },
+      { title: 'loan', image: 'https://storage.googleapis.com/gooloan-images/gooloan-4.jpg', description: '的搜索結果集中找到該頁面，則社交個人資料標記應該創建一個或多個麵包屑路徑，而不會有' },
     ];
   }
 
-  render() {
-    return html`
+    render() {
+      return html`
       <hello-lit
         .author=${this.author}
         .items=${this.items}>
       </hello-lit>
     `;
-  }
+    }
 }
 
 customElements.define('my-app', MyApp);
