7f29bc11423a7df4ccb5edd94cf86ca8d9cbeeaf
iambite <wne@iambite.com>
2019-06-12 06:29:37 +0800

    1 file modified

--- src/hello-lit.js
+++ src/hello-lit.js
@@ -7,16 +7,21 @@ class HelloLit extends LitElement {
       name: { type: String }
     }
   }
-
   constructor() {
     super();
-    this.name = '';
+    this.author = { name: 'Gooloan', logo: 'http://www.myfreephotoshop.com/wp-content/uploads/2013/08/669.jpg' };
+    this.items = [
+      { title: 'loan', image: 'http://www.myfreephotoshop.com/wp-content/uploads/2013/08/669.jpg', description: '的搜索結果集中找到該頁面，則社交個人資料標記應該創建一個或多個麵包屑路徑，而不會有' },
+      { title: 'loan', image: 'http://www.myfreephotoshop.com/wp-content/uploads/2013/08/669.jpg', description: '的搜索結果集中找到該頁面，則社交個人資料標記應該創建一個或多個麵包屑路徑，而不會有'},
+    ];
   }
 
   render() {
     return html`
-      ${SharedStyles}
-      <h1>Hello ${this.name}!</h1>
+      <author-profile
+        .author=${this.author}
+        .items=${this.items}>
+      </author-profile>
     `;
   }
 }
